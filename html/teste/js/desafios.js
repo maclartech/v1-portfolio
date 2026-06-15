9f6f0a32b4972710bc96ae5f581860f48f5cb7ed
Maria Clara Carvalho <mclaracarvalho.tech@gmail.com>
2026-06-15 17:37:47 -0300

    error

--- html/teste/js/desafios.js
+++ html/teste/js/desafios.js
@@ -5,6 +5,6 @@ export const desafios = [
         icone: "fa-calculator",
         descricao: "Uma calculadora de terminal em Python com tratamento de erros (try/except), laços de repetição e operações avançadas.",
         tecnologias: ["Python", "CLI", "Lógica"],
-        github: "https://github.com/maclartech/52-desafios-ads/blob/main/1-periodo-logica/calculadoraCLI.py"
+        github: "https://github.com/maclartech/52-desafios-ads/blob/main/1-periodo-logica/01-calculadora-cli/%DD%81%20%CB%96%E1%B2%98%F0%90%91%BC%E2%8B%86.py"
     }
 ];
